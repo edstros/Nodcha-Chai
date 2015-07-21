09de23ec6e45381b380d6ca4a73c9b3688549bc3
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-21 12:11:23 -0500

    addtional tests

--- test/test.js
+++ test/test.js
@@ -8,52 +8,58 @@ describe('Tests', function () {
     true.should.equal(true);
   });
 });
-describe('Animal', function(){
-  describe('constructor', function(){
-    it('should return an animal object', function(){
+describe('Animal', function () {
+  describe('constructor', function () {
+    it('should return an animal object', function () {
       var animal = new Animal();
       animal.should.be.an('object');
       animal.should.be.an.instanceOf(Animal);
     });
-    it('should be alive', function (){
+    it('should be alive', function () {
       var animal = new Animal();
       animal.isAlive.should.be.true;
-      //isAlive defined in /lib/Animal.js
+      //defined in Animal.js
     });
+    it('should accept a type', function () {
+      var cat = new Animal('cat');
+      var dog = new Animal('dog');
+      cat.type.should.equal('cat');
+      dog.type.should.equal('dog');
+    })
   });
-
-  describe('#beCute()', function(){
-    it('should be a prototype method', function(){
+  describe('#beCute()', function () {
+    it('should be a prototype method', function () {
       var animal = new Animal();
       animal.should.respondTo('beCute');
       animal.should.not.have.ownProperty('beCute');
     });
-
-    it('should make the animal cute', function(){
+    it('should make the animal cute', function () {
       var animal = new Animal();
       should.not.exist(animal.isCute);
+      //no cute animals until beCute
       animal.beCute();
       animal.isCute.should.be.true;
     });
-
   });
 });
 describe('Array', function () {
-  describe('#filter()', function(){
-    it('should return an array of items that return truthy in the innner fn', function(){
-     var array = [1,2,3,4,5];
-      var output = array.filter(function (item){
+  describe('#filter()', function () {
+    it('should return an array of items that return truthy in the innner fn', function () {
+      var array = [1, 2, 3, 4, 5];
+      var output = array.filter(function (item) {
         return item % 2;
-      });output.should.eql([1,3,5]);
+        //filter out even numbers
+      });
+      output.should.eql([1, 3, 5]);
     });
   });
   describe('#map()', function () {
-    it('should create an array with the return value of the inner fn', function(){
-      var array = [1,2,3,4,5];
-      var output = array.map(function(item){
+    it('should create an array with the return value of the inner fn', function () {
+      var array = [1, 2, 3, 4, 5];
+      var output = array.map(function (item) {
         return item * item;
       });
-      output.should.eql([1,4,9,16,25]);
+      output.should.eql([1, 4, 9, 16, 25]);
     });
     it('should keep the same length', function () {
       var array = [1, 2, 3, 4, 5];
@@ -62,12 +68,12 @@ describe('Array', function () {
       }).length;
       length.should.equal(array.length);
     });
-  it('should not mutate the original array', function (){
-    var array = [1,2,3,4,5];
-    array.map(function (){
-      return false;
+    it('should not mutate the original array', function () {
+      var array = [1, 2, 3, 4, 5];
+      array.map(function () {
+        return false;
+      });
+      array.should.eql([1, 2, 3, 4, 5]);
     });
-  array.should.eql([1,2,3,4,5]);
-  });
   });
 });

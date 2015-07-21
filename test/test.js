ab35b35678ec492b92c6b0a07a9db5aacda085fc
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-21 14:25:04 -0500

    CLI test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var should = require('chai').should();
 var Animal = require(path.join(process.cwd() + '/lib/Animal'));
+var cp = require('child_process');
 //process.cwd goes to the root dir, so it can go to any directory from the root
 //path.join gives you a proper path, but need to be defined
 describe('Tests', function () {
@@ -8,6 +9,21 @@ describe('Tests', function () {
     true.should.equal(true);
   });
 });
+
+describe.only('CLI', function(){
+  it('should thank me for downloading', function (done){
+    console.log('childprocess');
+    cp.execFile('./app.js', function (err, stdout) {
+      console.log('err', err);
+      console.log('stdout', stdout);
+      stdout.should.equal('Thanks for downloading my app!\n');
+      //fails when console.log() from app.js has different output
+      done();
+    });
+  })
+});
+
+
 describe('Animal', function () {
   describe('constructor', function () {
     it('should return an animal object', function () {
